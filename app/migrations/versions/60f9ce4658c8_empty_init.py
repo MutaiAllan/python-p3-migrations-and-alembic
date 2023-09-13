496a5f93e2f3a56892094ff1af82e97e959729f8
MutaiAllan <MutaiAllan>
2023-09-13 20:46:16 +0300

    Added codes for migration

---
+++ app/migrations/versions/60f9ce4658c8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 60f9ce4658c8
+Revises: 
+Create Date: 2023-09-13 20:31:39.597548
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '60f9ce4658c8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
